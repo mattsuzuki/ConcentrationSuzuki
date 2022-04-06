51e6d0395c20ee0123a381df1534a9a0c540ea2b
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-04-06 08:47:12 -0700

    Total revamp of HTML, CSS, and JS

--- js/main.js
+++ js/main.js
@@ -1,122 +1,80 @@
 /*----- constants -----*/
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max) + 1;
-  }
-
-let playcards = [
-  1,
-  1,
-  2,
-  2,
-  3,
-  3,
-  4,
-  4,
-  5,
-  5,
-  6,
-  6,
-  7,
-  7,
-  8,
-  8,
-  9,
-  9,
-  10,
-  10,
-  "j",
-  "j",
-  "q",
-  "q",
+// Each of the "card" objects will be used twice,
+// then shuffled and used for the board's cards
+var SOURCE_CARDS = [
+  {img: 'https://i.imgur.com/ZXPKaiN.jpg', matched: false},
+  {img: 'https://i.imgur.com/XMEsZBX.jpg', matched: false},
+  {img: 'https://i.imgur.com/6jX1bMT.jpg', matched: false},
+  {img: 'https://i.imgur.com/yKdqsBv.jpg', matched: false},
+  {img: 'https://i.imgur.com/1BV3HLr.jpg', matched: false},
+  {img: 'https://i.imgur.com/QYmN6Hp.jpg', matched: false},
+  {img: 'https://i.imgur.com/D5pWE05.jpg', matched: false},
+  {img: 'https://i.imgur.com/Ss4Xo3x.jpg', matched: false}
 ];
+const CARD_BACK = 'https://i.imgur.com/WoEmI2M.pg'; 
 
 /*----- app's state (variables) -----*/
-
-
+let cards;  // Array of 16 shuffled card objects
+let firstCard;  // First card clicked (card object) or null
+let numBad;
+let ignoreClicks;
 
 /*----- cached element references -----*/
-const cards = document.querySelectorAll(".concentrate-card");
-const starts = document.querySelector(".play-reset");
-const gameboard = document.querySelector(".concentrate-game-board");
-const hides = document.querySelector('.concentrate-card');
+const msgEl = document.querySelector('h3');
 
 /*----- event listeners -----*/
-cards.forEach((card) => card.addEventListener("click", turnCard));
-starts.forEach((start) => start.addEventListener("click", startGame));
-hides.forEach((hide) => hide.addEventListener("click", hideGame));
-
+document.querySelector('main').addEventListener('click', handleChoice);
 
 /*----- functions -----*/
-function hideGame(){
- let x = document.querySelectorAll(".concentrate-card");
-  if (x.style.display === "none") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "none";
-  }
-}
+init();
 
-function startGame() {
-  shuffle(playcards);  
-}
-console.log(shuffle(cards));
-
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
-
-let playTurnedCard = false;
-let firstCard, secondTurn;
-
-function selectCard() {
-  this.classList.add('select');
-
-  if (!playTurnedCard) {
-    playTurnedCard = true;
-    firstCard = this;
-    console.log({playTurnedCard, firstCard});
-  }
-cards.forEach(card => card.addEventListener('click', selectCard));
-
-}
-
-function turnCard() {
-  this.classList.toggle("turn");
-  console.log("turned");
-  console.log(this);
+// Initialize all state, then call render()
+function init() {
+  cards = getShuffledCards();
+  firstCard = null;
+  numBad = 0;
+  ignoreClicks = false;
+  render();
 }
 
 function render() {
-  gameboard;
+  cards.forEach(function(card, idx) {
+    const imgEl = document.getElementById(idx);
+    const src = (card.matched || card === firstCard) ? card.img : CARD_BACK;
+    imgEl.src = src;
+  });
+  msgEl.innerHTML = `Bad Count: ${numBad}`;
 }
 
+function getShuffledCards() {
+  let tempCards = [];
+  let cards = [];
+  for (let card of SOURCE_CARDS) {
+    tempCards.push({...card}, {...card});
+  }
+  while (tempCards.length) {
+    let rndIdx = Math.floor(Math.random() * tempCards.length);
+    let card = tempCards.splice(rndIdx, 1)[0];
+    cards.push(card);
+  }
+  return cards;
+}
 
-let cardEls = document.querySelectorAll(".front-face");
-
-cardEls.forEach((card ) => {
-    let randomNum = getRandomInt(9);
-    let url = `./imgs/fronts/clubs_${randomNum}.svg`;
-    card.setAttribute("src", url);
-});
-
-
-
-
-
-
+// Update all impacted state, then call render()
+function handleChoice(evt) {
+  const cardIdx = parseInt(evt.target.id);
+  if (isNaN(cardIdx) || ignoreClicks) return;
+  const card = cards[cardIdx];
+  if (firstCard) {
+    if (firstCard.img === card.img) {
+      // correct match
+      firstCard.matched = card.matched = true;
+    } else {
+      numBad++;
+    }
+    firstCard = null;
+  } else {
+    firstCard = card;
+  }
+  render();
+}
